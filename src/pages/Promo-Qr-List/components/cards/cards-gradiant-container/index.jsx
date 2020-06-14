6a1a0c0819732b0cd61522b30248aa6cd6f220fa
Mar0903 <cristiangm96@hotmail.com>
2020-06-14 11:49:11 -0500

    [modificated] conditional rendering

--- src/pages/Promo-Qr-List/components/cards/cards-gradiant-container/index.jsx
+++ src/pages/Promo-Qr-List/components/cards/cards-gradiant-container/index.jsx
@@ -5,12 +5,12 @@ import './cards-gradiant-container.css'
 import { Link } from 'react-router-dom'
 
 export default function CardGradiantContainer (props) {
-  const { children, id } = props
+  const { children, id, isUser } = props
   return (
     <>
       {
         id ? (
-          <Link to={`/${id}`}>
+          <Link to={isUser ? `album-qr/${id}` : `/${id}`}>
             <div className='card-gradiant-container'>
               {children}
             </div>
