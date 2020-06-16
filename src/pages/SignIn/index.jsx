3498977f14fdd1c83c12fe8f71543d9a3b05f565
Mar0903 <cristiangm96@hotmail.com>
2020-06-15 21:19:11 -0500

    modificated to join to backend , and  redirect

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Header from '../../Components/Header'
 import Inputs from './components/inputs'
+import api from '../../lib/api'
+import { Redirect } from 'react-router-dom'
 
 // css
 import './SignIn.css'
@@ -9,16 +11,53 @@ export default class SignIn extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      fullName: '',
-      phoneNum: '',
+      name: '',
       email: '',
       password: '',
-      confirmPassword: '',
-      success: false
+      city: '',
+      age: '',
+      gender: '',
+      hasSignedUP: false,
+      isLogedIn: false
     }
+    this.handleInputOnChange = this.handleInputOnChange.bind(this)
+    this.handleButton = this.handleButton.bind(this)
+  }
+
+  componentDidMount () {
+    const token = localStorage.getItem('authUserToken')
+    this.setState({
+      isLogedIn: !!token
+    })
+  }
+
+  handleInputOnChange ({ target: { value, name } }) {
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleButton (event) {
+    event.preventDefault()
+    api.signup(this.state)
+      .then((user) => {
+        this.setState({
+          hasSignedUP: true
+        })
+      })
   }
 
   render () {
+    if (this.state.isLogedIn) {
+      return (
+        <Redirect to='/promos' />
+      )
+    }
+    if (this.state.hasSignedUP) {
+      return (
+        <Redirect to='/login' />
+      )
+    }
     return (
       <div className='container-signin'>
         <div className='row container-form'>
@@ -32,25 +71,25 @@ export default class SignIn extends Component {
           </div>
           <div className='col-12 col-md-8 p-3'>
             <form>
-              <Inputs labelName='name' labelFor='name' placeholder='name' min='3' type='text' />
-              <Inputs labelName='email' labelFor='email' placeholder='tu@email.com' min='3' type='email' />
-              <Inputs labelName='password' labelFor='password' placeholder='123456' min='8' type='password' />
-              <Inputs labelName='city' labelFor='city' type='text' />
-              <Inputs labelName='age' labelFor='age' max='83' min='18' type='number' />
-              <div className='row m-0 d-flex justify-content-center'>
+              <Inputs name='name' labelFor='name' placeholder='name' min='3' type='text' value={this.state.name} onChange={this.handleInputOnChange} />
+              <Inputs name='email' labelFor='email' placeholder='tu@email.com' min='3' type='email' value={this.state.email} onChange={this.handleInputOnChange} />
+              <Inputs name='password' labelFor='password' placeholder='123456' min='8' type='password' value={this.state.password} onChange={this.handleInputOnChange} />
+              <Inputs name='city' labelFor='city' type='text' value={this.state.city} onChange={this.handleInputOnChange} />
+              <Inputs name='age' labelFor='age' max='83' min='18' type='number' value={this.state.age} onChange={this.handleInputOnChange} />
+              <div className='row m-0 d-flex justify-content-between justify-content-md-center'>
                 <div className='col-2'>
-                  <Inputs labelName='female' labelFor='female' name='gender' type='radio' />
+                  <Inputs labelName='female' labelfor='female' name='gender' type='radio' onClick={this.handleInputOnChange} value='female' />
                 </div>
                 <div className='col-2'>
-                  <Inputs labelName='male' labelFor='male' name='gender' type='radio' />
+                  <Inputs labelName='male' labelfor='male' name='gender' type='radio' onClick={this.handleInputOnChange} value='male' />
                 </div>
                 <div className='col-2'>
-                  <Inputs labelName='noBinary' labelFor='nobinary' name='gender' type='radio' />
+                  <Inputs labelName='noBinary' labelfor='noBinary' name='gender' type='radio' onClick={this.handleInputOnChange} value='noBinary' />
                 </div>
               </div>
               <div className='row d-flex justify-content-center'>
                 <div className='col-6'>
-                  <button type='submit' className='m-3 px-4 button py-2'>SignIn</button>
+                  <button type='submit' className='m-3 px-4 button py-2' onClick={this.handleButton}>SignIn</button>
                 </div>
               </div>
             </form>
