753206e328be3c82616d78cb4d863f7d62a4c4dd
EdTorres <l.torres@supaada.mx>
2020-06-18 17:25:38 -0500

    NAV BAR 2DA VERSION

--- src/Components/NavBarAdmin/index.js
+++ src/Components/NavBarAdmin/index.js
@@ -50,7 +50,8 @@ export default class Login extends Component {
             viewBox="0 0 512 512"
             className="svg-inline--fa fa-cat fa-w-16 fa-9x"
          ></svg>*/}
-              <img src="https://img.icons8.com/nolan/64/bar-chart.png" className='rounded d-block mt-3 margenNav' alt='igsh'/>
+              
+              <img src="https://img.icons8.com/nolan/64/product.png" className='rounded d-block mt-3 margenNav' alt='igsh'/>
               <g className="fa-group">
                 <path
                   fill="currentColor"
@@ -65,7 +66,7 @@ export default class Login extends Component {
               ></path>
             </g>
           
-          <span className="link-text">PRODUCT DASHBOARD</span>
+          <span className="link-text">REGISTRAR PRODUCTO</span>
         </a>
       </li>
 
@@ -82,7 +83,8 @@ export default class Login extends Component {
             className="svg-inline--fa fa-alien-monster fa-w-18 fa-9x"
           ></svg>*/}
           
-           <img src="https://img.icons8.com/nolan/64/area-chart.png" className='rounded d-block mt-3 margenNav' alt='igsh'/>
+           
+           <img src="https://img.icons8.com/nolan/64/discount.png" className='rounded d-block mt-3 margenNav' alt='igsh'/>
             <g className="fa-group">
               <path
                 fill="currentColor"
@@ -96,7 +98,7 @@ export default class Login extends Component {
               ></path>
             </g>
           
-          <span className="link-text">BRAND DASHBOARD</span>
+          <span className="link-text">REGISTRAR PROMOCION</span>
         </a>
       </li>
 
@@ -113,7 +115,8 @@ export default class Login extends Component {
             className="svg-inline--fa fa-alien-monster fa-w-18 fa-9x"
           ></svg>*/}
            
-           <img src="https://img.icons8.com/nolan/64/menu.png" className='rounded d-block mt-3 margenNav' alt='igsh'/>
+           
+           <img src="https://img.icons8.com/nolan/64/shopping-cart-loaded.png" className='rounded d-block mt-3 margenNav' alt='igsh'/>
             <g className="fa-group">
               <path
                 fill="currentColor"
@@ -127,7 +130,37 @@ export default class Login extends Component {
               ></path>
             </g>
           
-          <span className="link-text">PROMOTIONS LIST</span>
+          <span className="link-text">LISTA DE PROMOCIONES</span>
+        </a>
+      </li>
+
+      <li className="nav-item">
+        <a href="#" className="nav-link">
+          {/*<svg
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fad"
+            data-icon="alien-monster"
+            role="img"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 576 512"
+            className="svg-inline--fa fa-alien-monster fa-w-18 fa-9x"
+          ></svg>*/}
+           <img src={sh} className='rounded d-block mt-3 margenNav' alt='igsh' />
+            <g className="fa-group">
+              <path
+                fill="currentColor"
+                d="M560,128H528a15.99954,15.99954,0,0,0-16,16v80H480V176a15.99954,15.99954,0,0,0-16-16H416V96h48a16.00079,16.00079,0,0,0,16-16V48a15.99954,15.99954,0,0,0-16-16H432a15.99954,15.99954,0,0,0-16,16V64H368a15.99954,15.99954,0,0,0-16,16v48H224V80a15.99954,15.99954,0,0,0-16-16H160V48a15.99954,15.99954,0,0,0-16-16H112A15.99954,15.99954,0,0,0,96,48V80a16.00079,16.00079,0,0,0,16,16h48v64H112a15.99954,15.99954,0,0,0-16,16v48H64V144a15.99954,15.99954,0,0,0-16-16H16A15.99954,15.99954,0,0,0,0,144V272a16.00079,16.00079,0,0,0,16,16H64v80a16.00079,16.00079,0,0,0,16,16h48v80a16.00079,16.00079,0,0,0,16,16h96a16.00079,16.00079,0,0,0,16-16V432a15.99954,15.99954,0,0,0-16-16H192V384H384v32H336a15.99954,15.99954,0,0,0-16,16v32a16.00079,16.00079,0,0,0,16,16h96a16.00079,16.00079,0,0,0,16-16V384h48a16.00079,16.00079,0,0,0,16-16V288h48a16.00079,16.00079,0,0,0,16-16V144A15.99954,15.99954,0,0,0,560,128ZM224,320H160V224h64Zm192,0H352V224h64Z"
+                className="fa-secondary"
+              ></path>
+              <path
+                fill="currentColor"
+                d="M160,320h64V224H160Zm192-96v96h64V224Z"
+                className="fa-primary"
+              ></path>
+            </g>
+          
+          <span className="link-text">SCAN QR</span>
         </a>
       </li>
       
