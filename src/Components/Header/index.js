aa886fcf7423f3b3595f739d87fc8eb1091e1a4b
Airy-Dev6 <61258585+Airy-Dev6@users.noreply.github.com>
2020-06-19 09:51:16 -0500

    Style nav and Add logout button (#14)

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -8,10 +8,17 @@ import {
 export default class Login extends Component {
   render () {
     return (
-      <div className='col-12 my-3'>
-        <Link to='/'>
-          <img src={sh} className='rounded mx-auto d-block mt-2' alt='igsh' />
-        </Link>
+      <div className='row'>
+        <div className='col-5 my-3 '>
+          <Link to='/'>
+            <img src={sh} className='rounded mx-auto d-block mt-2' alt='igsh' />
+          </Link>
+        </div>
+        <div className='col-3' />
+        <div className='col-3 my-3'>
+          <button type='submit' className='m-3 px-4 btn btn-outline-secondary py-2'>CERRAR SESIÓN</button>
+        </div>
+        <div className='col-1' />
       </div>
     )
   }
